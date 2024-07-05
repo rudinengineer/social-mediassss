f41a9ccf7ae088e882a5ea94055d461b703d3cb5
rudinengineer <114669073+rudinengineer@users.noreply.github.com>
2024-07-05 15:38:34 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,44 @@
-// import { withAuth } from "next-auth/middleware"
-// import { NextResponse } from "next/server"
-// import { AUTH_PATH, PROTECTED_PATH, PUBLIC_PATH, ROOT_PATH } from "./lib/route"
+import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
+import { AUTH_PATH, PROTECTED_PATH, PUBLIC_PATH, ROOT_PATH } from "./lib/route"
 
-// // export const config = { matcher: ["/dashboard/", "/dashboard/:path*"] };
+// export const config = { matcher: ["/dashboard/", "/dashboard/:path*"] };
+
+export default withAuth(
+    function middleware(req) {
+        if ( AUTH_PATH.includes(req.nextUrl.pathname) && !req.nextauth.token ) {
+            return NextResponse.redirect(new URL('/signin', req.nextUrl))
+        }
+        if ( PROTECTED_PATH.includes(req.nextUrl.pathname) ) {
+            if ( req.nextauth.token ) {
+                return NextResponse.redirect(new URL(ROOT_PATH, req.nextUrl))
+            }
+        } else {
+            if ( req.nextUrl.pathname !== ROOT_PATH && !PUBLIC_PATH.includes(req.nextUrl.pathname) && !req.nextauth.token ) {
+                return NextResponse.redirect(new URL('/signin', req.nextUrl))
+            }
+        }
+    },
+    {
+        secret: process.env.NEXT_AUTH_SECRET!,
+    }
+)
 
 // export default withAuth(
-//     function middleware(req) {
-//         if ( AUTH_PATH.includes(req.nextUrl.pathname) && !req.nextauth.token ) {
-//             return NextResponse.redirect(new URL('/signin', req.nextUrl))
-//         }
-//         if ( PROTECTED_PATH.includes(req.nextUrl.pathname) ) {
-//             if ( req.nextauth.token ) {
-//                 return NextResponse.redirect(new URL(ROOT_PATH, req.nextUrl))
-//             }
-//         } else {
-//             if ( req.nextUrl.pathname !== ROOT_PATH && !PUBLIC_PATH.includes(req.nextUrl.pathname) && !req.nextauth.token ) {
-//                 return NextResponse.redirect(new URL('/signin', req.nextUrl))
-//             }
-//         }
-//     },
 //     {
+//         callbacks: {
+//             authorized({ token }) {
+//                 if ( token ) {
+//                     return true
+//                 }
+//                 return false
+//             },
+//         },
 //         secret: process.env.NEXT_AUTH_SECRET!,
+//         pages: {
+//             signIn: '/signin',
+//             newUser: '/signup',
+//             signOut: '/auth/logout'
+//         }
 //     }
 // )
-
-// // export default withAuth(
-// //     {
-// //         callbacks: {
-// //             authorized({ token }) {
-// //                 if ( token ) {
-// //                     return true
-// //                 }
-// //                 return false
-// //             },
-// //         },
-// //         secret: process.env.NEXT_AUTH_SECRET!,
-// //         pages: {
-// //             signIn: '/signin',
-// //             newUser: '/signup',
-// //             signOut: '/auth/logout'
-// //         }
-// //     }
-// // )
