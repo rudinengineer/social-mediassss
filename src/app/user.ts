38fa4ff509ba6e09c711325e035fdcf32ea706cc
rudinengineer <114669073+rudinengineer@users.noreply.github.com>
2024-07-08 13:36:40 +0700

    Delete src directory

--- src/app/user.ts
+++
@@ -1,9 +0,0 @@
-export type User = {
-    id: number,
-    username: string,
-    password: string,
-    email: string,
-    image: string
-    createdAt: Date,
-    updatedAt: Date
-}
--- src/lib/route.ts
+++
@@ -1,4 +0,0 @@
-export const ROOT_PATH = '/'
-export const PROTECTED_PATH = ['/account']
-export const AUTH_PATH = ['/signup', '/signin']
-export const PUBLIC_PATH = ['/']
